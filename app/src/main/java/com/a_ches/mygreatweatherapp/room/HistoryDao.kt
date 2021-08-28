9f2828e9de2b70d9f5a229ce9b2c562008d82d54
APopova <a_ches@mail.ru>
2021-08-28 15:23:08 +0300

    HW_9_v1

--- app/src/main/java/com/a_ches/mygreatweatherapp/room/HistoryDao.kt
+++ app/src/main/java/com/a_ches/mygreatweatherapp/room/HistoryDao.kt
@@ -2,6 +2,9 @@ package com.a_ches.mygreatweatherapp.room
 
 import androidx.room.*
 
+import android.database.Cursor
+import androidx.room.*
+
 @Dao
 interface HistoryDao {
     @Query("SELECT * FROM HistoryEntity")
@@ -18,4 +21,13 @@ interface HistoryDao {
 
     @Delete
     fun delete(entity: HistoryEntity)
+
+    @Query("DELETE FROM HistoryEntity WHERE id = :id")
+    fun deleteById(id: Long)
+
+    @Query("SELECT id, city, temperature FROM HistoryEntity")
+    fun getHistoryCursor(): Cursor
+
+    @Query("SELECT id, city, temperature FROM HistoryEntity WHERE id = :id")
+    fun getHistoryCursor(id: Long): Cursor
 }
